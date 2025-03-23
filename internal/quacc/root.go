264e59bc5008bbd06a609e9954e567c98d9a3c81
CharukaK <charukakarunanayake@gmail.com>
2025-03-23 07:52:28 +0530

    Setup note base at the init

--- internal/quacc/root.go
+++ internal/quacc/root.go
@@ -2,6 +2,10 @@ package quacc
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -11,11 +15,14 @@ type RootCmdOpts struct {
 }
 
 var cmdOpts = &RootCmdOpts{}
+var noteBase = ""
+var editor = ""
 
 var rootCmd = &cobra.Command{
 	Use:   `quacc`,
 	Short: `quick access notes`,
 	Long:  `Quick Access Notes`,
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
 			return fmt.Errorf("no topic provided")
@@ -37,9 +44,43 @@ func handleEdit(args []string) error {
 		return fmt.Errorf("missing arguments")
 	}
 
-	return nil
+	p, _ := parseArguments(args[0])
+
+	fp := path.Join(noteBase, p)
+
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		nFile := fmt.Sprintf(`%s.md`, fp)
+		fmt.Println(nFile)
+		file, err := os.Create(nFile)
+
+		if err != nil {
+			return err
+		}
+
+		file.Close()
+	}
+
+	cmd := exec.Command(editor, fp)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
 }
 
+func parseArguments(input string) (path string, searchQuery []string) {
+	segments := strings.Split(input, "~")
+
+	if len(segments) > 0 {
+		path = segments[0]
+	}
+
+	if len(segments) > 1 {
+		searchQuery = strings.Split(segments[1], "+")
+	}
+
+	return
+}
 
 func RunCmd() {
 	if err := rootCmd.Root().Execute(); err != nil {
@@ -50,4 +91,15 @@ func RunCmd() {
 
 func init() {
 	rootCmd.Flags().BoolVarP(&cmdOpts.isEdit, "edit", "e", false, "")
+
+	// set note base
+	// todo: make this to be read from a config file later
+
+	if dir, err := setupBaseDir(); err != nil {
+		// todo: handle error
+	} else {
+		noteBase = dir
+	}
+
+	editor, _ = os.LookupEnv("EDITOR")
 }
